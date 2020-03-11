907ca6b774dfcaa4bf0ba9f0002e5797f8a78703
alexandre-roque <61557947+alexandre-roque@users.noreply.github.com>
2020-03-11 20:59:31 -0300

    Add files via upload

---
+++ Relogio/src/Contador.java
@@ -0,0 +1,36 @@
+public class Contador {
+    private int valor;
+    private double limite;
+
+    public Contador(int valor, double limite) {
+        this.valor = valor;
+        this.limite = limite;
+    }
+    
+    public Contador(){}
+    
+    public int getValor() {
+        return valor;
+    }
+
+    public void setValor(int valor) {
+        this.valor = valor;
+    }
+
+    public double getLimite() {
+        return limite;
+    }
+
+    public void setLimite(double limite) {
+        this.limite = limite;
+    }
+    
+    public void incrementar(){
+        /*for(int i=0;i<limite;i++){
+            
+        }*/
+        valor++;
+        
+    }
+    
+}
